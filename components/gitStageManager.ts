dbc0281df047a1418a652b1a7d6a403fa999f2ed
Braian A. Diez <bdiez19@gmail.com>
2024-07-27 11:40:09 -0300

    feat: Add simple-git npm package for Git operations
    The commit adds the "simple-git" npm package as a dependency in the "package.json" file. This package is used in the "gitStageManager.ts" module for performing Git operations such as adding and resetting staged files. The "simple-git" package provides a simple and convenient API for interacting with Git repositories.

--- components/gitStageManager.ts
+++ components/gitStageManager.ts
@@ -1,33 +1,62 @@
-import { $ } from "bun";
+import { simpleGit, type SimpleGit, type SimpleGitOptions } from "simple-git";
 import { updateGlobalFilesContent } from "stageScrapper";
 
-const gitStatusOutputText = await $`git status --porcelain | awk '{print substr($0, 4)}'`.text();
-const files = gitStatusOutputText.trim().split('\n');
+const options: Partial<SimpleGitOptions> = {
+  baseDir: process.cwd(),
+  binary: 'git',
+  maxConcurrentProcesses: 6,
+  trimmed: false,
+};
+const git: SimpleGit = simpleGit(options);
 
-const fileOptions = files.map(file => {
-  return { value: file, label: file };
-});
+async function getFileOptions() {
+  const status = await git.status();
+  const fileOptions = status.files.map(file => ({ value: file.path, label: file.path }));
+  return fileOptions;
+}
 
 async function addStagedFiles(stagedFiles: string[]) {
   console.log("Agregando archivos seleccionados:", stagedFiles);
   for (const file of stagedFiles) {
-    await $`git add ${file}`;
+    try {
+      console.log(`Agregando archivo: ${file}`);
+      await git.add(file);
+      console.log(`Archivo agregado: ${file}`);
+    } catch (error) {
+      console.error(`Error al agregar el archivo ${file}:`, error);
+      throw error;
+    }
+  }
+
+  try {
+    const status = await git.status();
+    console.log("Estado de git después de agregar archivos:", status);
+    return stagedFiles;
+  } catch (error) {
+    console.error("Error al obtener el estado de git:", error);
+    throw error;
   }
 }
 
 async function resetStagedFiles(files: string[]) {
   console.log("Reseteando archivos seleccionados:", files);
   for (const file of files) {
-    await $`git reset ${file}`;
+    try {
+      await git.reset(['--', file]);
+      console.log(`Archivo reseteado: ${file}`);
+    } catch (error) {
+      console.error(`Error al resetear el archivo ${file}:`, error);
+      throw error;
+    }
   }
 }
 
 async function getFileContent(stagedFiles: string[]): Promise<string> {
   console.log("Obteniendo contenido de los archivos seleccionados:", stagedFiles);
   const content = await updateGlobalFilesContent(stagedFiles);
-  console.log("Contenido de los archivos obtenidos:", content);
   return content;
 }
 
+const fileOptions = await getFileOptions();
 
 export { fileOptions, addStagedFiles, resetStagedFiles, getFileContent };
