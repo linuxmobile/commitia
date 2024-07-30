5e3a772f481d53cf20fdaa311ac16cc00dc34d75
Braian A. Diez <bdiez19@gmail.com>
2024-07-30 14:13:24 -0300

    fix: Remove redundant directories and files in ~/.local/share/bin/ and create necessary directories.

--- install.sh
+++ install.sh
@@ -12,6 +12,12 @@ then
 fi
 
 if bun build commitia.js --outdir=dist/ --format=esm --external=i18xs --external=picocolors --sourcemap; then
+    if [ -d ~/.local/share/bin/ ]; then
+        rm -rf ~/.local/share/bin/commitia
+        rm -rf ~/.local/share/bin/locales
+    else
+        mkdir -p ~/.local/share/bin/
+    fi
     cp -rf dist/commitia.js ~/.local/share/bin/commitia
     cp -rf locales/ ~/.local/share/bin/
 else
